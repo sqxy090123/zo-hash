7844e583ae5e995a28d943d8413a3e49bf50f206
sqxy090123 <sqx20150423@outlook.com>
2026-03-22 14:12:21 +0800

    hash logic improvement

--- zohash/__init__.py
+++ zohash/__init__.py
@@ -1,7 +1,15 @@
 """
-ZO-Hash 模块
-提供基于海绵结构的256位哈希算法，以及ZO编码的哈希表示。
-支持内存数据和文件的哈希计算。
+# ZO-Hash 模块
+## 提供基于海绵结构的256位哈希算法，以及ZO编码的哈希表示。
+## 支持内存数据和文件的哈希计算。
+usage:
+```
+>>> import zohash
+>>> zohash.dy512("C:/path/to/your/file") # on win
+'2t4ggPh.IVQqpZhFO.43TYYlF3pNSV9XXlm7Cuhai.Fsgr2Xwn495j402JJQv7EY1Ypg8FJWCWeLsjpxXlAZ6h'
+>>> zohash.dy512("/path/to/your/file") # on linux
+'2t4ggPh.IVQqpZhFO.43TYYlF3pNSV9XXlm7Cuhai.Fsgr2Xwn495j402JJQv7EY1Ypg8FJWCWeLsjpxXlAZ6h'
+```
 """
 
 from ._hash import filehash_256v1, memoryhash_256v2
